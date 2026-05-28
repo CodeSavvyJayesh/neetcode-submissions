ce3acfd9764549588b31383792bba726314bc4e2
Jayesh Dhamal <jayeshdhamal327@gmail.com>
2026-05-28 21:11:15 +0530

    Add: length-of-last-word - submission-3

---
+++ Data Structures & Algorithms/length-of-last-word/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int count = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+             char ch = s.charAt(i);
+             if(ch==' ' && count==0  )
+             {
+                 continue;
+             }
+             else if(ch==' ')
+             {
+                break;
+             }
+             else
+             {
+                 count++;
+             }
+        }
+        return count;
+    }
+}
